50771f87b90302561427cb2743d4b6c165cdd459
kazak <kazakova.katya@outlook.com>
2026-01-29 14:56:43 +0700

    single apple generates

--- apple_eating_game.py
+++ apple_eating_game.py
@@ -1,7 +1,16 @@
 import curses
 from time import sleep
+from random import randint
+
+def generate_apple():
+    x = randint(start_x + 1, board_width + start_x)
+    y = randint(start_y + 1, board_height + start_y)
+
+    return x, y
 
 def main(window):
+    global start_x, start_y, board_height, board_width
+
     curses.curs_set(0)
 
     start_x, start_y = 2, 2
@@ -12,20 +21,25 @@ def main(window):
 
     character = 'x'
     apple = '@'
+    score = 0
+
+    apple_x, apple_y = generate_apple()
 
     while True:
         window.clear()
 
+        # print board
         window.addstr(start_y, start_x, f"+{'-'*board_width}+")
         window.addstr(start_y+1+board_height, start_x, f"+{'-'*board_width}+")
         for row in range(1, board_height+1):
             window.addstr(start_y+row, start_x, f"|")
             window.addstr(start_y+row, start_x+board_width+1, f"|")
 
+        window.addch(apple_y, apple_x, apple)
         window.addstr(character_y, character_x, character)
         window.refresh()
 
-        window.addstr(0, 0, f"({character_x}, {character_y})")
+        window.addstr(0, 0, f"({character_x}, {character_y}) Score: {score}")
 
         pressed_key = window.getch()
         if pressed_key == ord('q'):
