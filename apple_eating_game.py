1bd24852187bd4dd53f4bc8e59152239e7e62c93
kazak <kazakova.katya@outlook.com>
2026-01-29 14:41:48 +0700

    moving character, drawn board

---
+++ apple_eating_game.py
@@ -0,0 +1,47 @@
+from zipapp import create_archive
+
+from board_print import create_board
+import curses
+from time import sleep
+
+def main(window):
+    curses.curs_set(0)
+
+    start_x, start_y = 2, 2
+    board_width, board_height = 16, 8
+
+    character_x = start_x + board_width//2
+    character_y = start_y + board_height//2
+
+    while True:
+        window.clear()
+
+        window.addstr(start_y, start_x, f"+{'-'*board_width}+")
+        window.addstr(start_y+1+board_height, start_x, f"+{'-'*board_width}+")
+        for row in range(1, board_height+1):
+            window.addstr(start_y+row, start_x, f"|")
+            window.addstr(start_y+row, start_x+board_width+1, f"|")
+
+        window.addstr(character_y, character_x, 'x')
+        window.refresh()
+
+        window_height, window_width = window.getmaxyx()
+        window.addstr(0, 0, f"({character_x}, {character_y})")
+
+        pressed_key = window.getch()
+        if pressed_key == ord('q'):
+            window.addstr(0, 0, 'thank you for playing the game')
+            window.refresh()
+            sleep(1)
+            break
+        elif pressed_key == ord('w') and character_y > 0:
+            character_y -= 1
+        elif pressed_key == ord('r') and character_y < window_height - 1:
+            character_y += 1
+        elif pressed_key == ord('a') and character_x > 0:
+            character_x -= 1
+        elif pressed_key == ord('s') and character_x < window_width - 3:
+            character_x += 1
+
+
+curses.wrapper(main)
