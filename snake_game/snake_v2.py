97e55bff46a803307cc547e0583f1033977a469f
kazak <kazakova.katya@outlook.com>
2026-02-05 10:58:50 +0700

    tail follows head

--- snake_game/snake_v2.py
+++ snake_game/snake_v2.py
@@ -2,7 +2,8 @@ import curses
 from time import sleep
 
 def main(window):
-    global board_h, board_w, board, player_x, player_y, snake_length, current_rotation
+    global board_h, board_w, board, player_x, player_y, \
+        snake_length, current_rotation
 
     curses.start_color()
     curses.use_default_colors()
@@ -10,13 +11,15 @@ def main(window):
 
     curses.init_pair(1, curses.COLOR_MAGENTA, -1)
     curses.init_pair(2, 237, -1)
+    curses.init_pair(3, curses.COLOR_YELLOW, -1)
 
     board_h, board_w = 16, 16
     player = '██'
     player_x, player_y = board_w-1, board_h//2
 
-    body = '##'
-    snake_length = 1
+    body = '██'
+    snake_length = 3
+    tail = [(player_x, player_y+(snake_length-i)) for i in range(1, snake_length)]
 
     #               up     right    down    left
     directions = [(0, -1), (2, 0), (0, 1), (-2, 0)]
@@ -44,6 +47,12 @@ def main(window):
         elif key == ord('p'): reset()
 
         if current_rotation != -1:
+            board.addstr(tail[0][1], tail[0][0], '  ')
+
+            for i in range(0, snake_length-2):
+                tail[i] = tail[i+1]
+
+            tail[-1] = (player_x, player_y)
             player_x, player_y = player_x + directions[current_rotation][0], player_y + directions[current_rotation][1]
 
         if (player_x < 1 or player_x >= board_w*2-2) or (player_y < 1 or player_y >= board_h-1):
@@ -53,8 +62,11 @@ def main(window):
             sleep(0.5)
             reset()
 
+        for i in range(0, snake_length-1):
+            board.addstr(tail[i][1], tail[i][0], body, curses.color_pair(3))
         board.addstr(player_y, player_x, player, curses.color_pair(1))
-        window.addstr(0, 0, f'{(player_x, player_y)},  rotation: {current_rotation}, flag: ')
+        window.addstr(0, 0, f'{(player_x, player_y)},  rotation: {current_rotation}    ')
+        window.addstr(0, 0, f'tail: {tail}')
 
         window.refresh()
         board.refresh()
